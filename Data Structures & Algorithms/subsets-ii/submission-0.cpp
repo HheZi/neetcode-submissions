feaa5790d2b971bef692aa9a8c574d7e9cdc92a3
Tseva Pavlo <144174871+HheZi@users.noreply.github.com>
2026-09-26 21:41:09 +0300

    Add: subsets-ii - submission-0

---
+++ Data Structures & Algorithms/subsets-ii/submission-0.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    vector<vector<int>> subsetsWithDup(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
+        vector<vector<int>> subsets;
+        vector<int> currSet;
+        helper(0, nums, subsets, currSet);
+        return subsets;
+    }
+
+    void helper(
+        int i, vector<int> &nums, 
+        vector<vector<int>> &subsets, vector<int> &currSet
+    ) {
+        if (i >= nums.size()) {
+            subsets.push_back(vector<int>(currSet));
+            return;
+        }
+
+        currSet.push_back(nums[i]);
+        helper(i + 1, nums, subsets, currSet);
+        currSet.pop_back();
+
+        while (i + 1 < nums.size() && nums[i] == nums[i + 1]) 
+            i++;
+        helper(i + 1, nums, subsets, currSet);
+    }
+};
